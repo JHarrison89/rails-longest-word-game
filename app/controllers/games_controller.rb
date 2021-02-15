d7055ddcf54c0153b31ec9e6973c190592ab5cb3
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2021-02-15 18:14:02 +0000

    chekcing if letters included & answer is a word

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
     @alpha = ('a'..'z').to_a
@@ -8,10 +11,26 @@ class GamesController < ApplicationController
   end
 
   def score
+    @answerString = params[:answer]
+    @answer = params[:answer].split("")
+    @letters = params[:letters].split(" ")
+
+    if english_word?(@answerString) && included?(@answer, @letters)
+      @result = 'zing!'
+    else
+      @result = 'no'
+    end
+  end
+
+  private
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+
+  def included?(answer, letters)
+    @answer.all? { |x| @letters.include?(x) }
   end
 end
-
-
-# Generate apla
-# Select random letter & save into array
-# repeate 10 times
